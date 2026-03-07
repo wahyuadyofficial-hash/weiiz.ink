28942eba058e66b8c4659018b66fe37414f19d78
Wahyu Ady Putra <wahyuady.official@gmail.com>
2026-03-08 01:13:01 +0800

    fix: remove nextauth route

--- app/api/auth/[...nextauth]/route.ts
+++
@@ -1,9 +0,0 @@
-// app/api/auth/[...nextauth]/route.ts
-// Hanya export GET dan POST — authOptions ada di lib/auth.ts
-
-import NextAuth from 'next-auth'
-import { authOptions } from '@/lib/auth'
-
-const handler = NextAuth(authOptions)
-
-export { handler as GET, handler as POST }
